51c2631f2d302b9b9ff786e5c218571c20a7639e
Sandeep Kumar Jena <sandeepkumar637053@gmail.com>
2023-07-27 17:15:03 +0530

    commit milestone projects

---
+++ Project 4/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.html"],
+  theme: {
+    extend: {backgroundImage:{ 'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',}},
+  },
+  plugins: [],
+}
+
